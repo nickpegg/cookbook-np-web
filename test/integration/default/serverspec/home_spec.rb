5650bce2149e55b38a02d9a473371843955e8896
Nick Pegg <nick@nickpegg.com>
2015-11-14 14:47:47 -0800

    Add some more serverspec tests

--- test/integration/default/serverspec/home_spec.rb
+++ test/integration/default/serverspec/home_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe 'np-web::home' do
+  describe user 'home' do
+    it { is_expected.to exist }
+  end
+
   describe file '/srv/web/home.nickpegg.com' do
     it { is_expected.to exist }
     it { is_expected.to be_directory }
     it { is_expected.to be_owned_by 'home' }
     it { is_expected.to be_grouped_into 'home' }
+  end
+
+  describe file '/srv/web/home.nickpegg.com/.virtualenv' do
+    it { is_expected.to be_directory }
+  end
+
+  describe service 'home.nickpegg.com' do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
   end
 
   describe process 'python' do
