6a47b337e3101b0bccb83823e05087cc9b87e7e7
Nick Pegg <nick@nickpegg.com>
2015-11-21 17:00:20 -0800

    Listen on IPv6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nick@nickpegg.com'
 license 'all_rights'
 description 'Sets up websites that I host'
 long_description 'Sets up websites that I host'
-version '0.2.0'
+version '0.2.1'
 
 depends 'application_python', '~> 4.0.0'
 depends 'nginx', '~> 2.7.6'
