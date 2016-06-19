f29e111774cc45f7ee7010d6b652cce7cec6ec2f
Nick Pegg <nick@nickpegg.com>
2016-06-18 21:07:17 -0700

    get rid of home stuff, that lives in another cookbook now

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,5 @@ default[:np_web][:group]    = 'www-data'
 # Array of static sites to set up
 default[:np_web][:static_sites] = []
 
-default[:np_web][:home][:repo] = 'https://github.com/nickpegg/home.git'
-default[:np_web][:home][:port] = 8001
-
 # nginx cookbook overrides
 default['nginx']['default_root'] = ::File.join(node[:np_web][:base_dir], 'default')
--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,8 @@ description 'Sets up websites that I host'
 long_description 'Sets up websites that I host'
 version '0.2.5'
 
-depends 'application_python', '~> 4.0.0'
-depends 'apt', '~> 2.9.2'
 depends 'nginx', '~> 2.7.6'
 depends 'nginx_vhost', '~> 0.1.0'
-depends 'poise-service', '~> 1.0.2'
-depends 'rabbitmq', '~> 4.4.0'
 
 supports 'debian', '~> 8.0'
 supports 'ubuntu', '= 14.04'
