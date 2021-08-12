7a9c960ff16df87b73c08b467d3645c33d380edc
Nick Pegg <nick@nickpegg.com>
2021-08-11 19:18:30 -0700

    bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Nick Pegg'
 maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up websites that I host'
-version '0.2.6'
+version '0.2.7'
 
 depends 'nginx', '~> 10.0'
 
