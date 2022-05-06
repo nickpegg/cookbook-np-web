6e2eea50607c43af915fff5c23ccd2c621740a26
Nick Pegg <nick@nickpegg.com>
2022-05-06 12:26:41 -0700

    Cookstyle fixes and other updates
    - Remove deb 10 support
    - Pin to Chef <18 until we get off nginx cookbook 10.x

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,12 @@ maintainer 'Nick Pegg'
 maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up websites that I host'
-version '0.2.8'
+version '0.2.9'
+
+# Need to upgrade nginx cookbook before we can support 18.x
+chef_version '< 18.0'
 
 depends 'nginx', '~> 10.0'
 
-supports 'debian', '~> 10.0'
 supports 'debian', '~> 11.0'
 supports 'ubuntu', '= 20.04'
