a051c979d0a68c08ad5f2be1852c08cfe74b743a
Nick Pegg <nick@nickpegg.com>
2022-05-07 17:47:02 -0700

    add ubuntu 22.04 support

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Nick Pegg'
 maintainer_email 'nick@nickpegg.com'
 license 'MIT'
 description 'Sets up websites that I host'
-version '0.2.9'
+version '0.2.10'
 
 # Need to upgrade nginx cookbook before we can support 18.x
 chef_version '< 18.0'
@@ -12,3 +12,4 @@ depends 'nginx', '~> 10.0'
 
 supports 'debian', '~> 11.0'
 supports 'ubuntu', '= 20.04'
+supports 'ubuntu', '= 22.04'
